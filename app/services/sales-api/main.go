c93abbc1125a2efc20a5468c7d6f7ff14023af51
ameghdadian <amirthedevops@gmail.com>
2024-09-16 11:02:58 +0330

    add docker package to support running test on a docker container

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -8,19 +8,13 @@ import (
 )
 
 /*
-	Next Steps:
-		1. Logger fmt 						DONE
-		2. Middlewares(start with Logger)
-*/
-
-/*
 	Requirements:
-	1. We need following signature for our handlers. They must be accepting context
+	1. In order to support timeouts, we need following signature for our handlers. They must be accepting context
 	   and return an error. Inside the program, errors needs to be propagated up the chain.
 	   This is required as we need to integrate error handling logic in a single
 	   place which is inside the Error middleware.
-	2. Neither http package HandlerFunc nor httptreemux provides the signature. We need
-	   a little bit of customization to satisfy our requirements.
+	Neither http package HandlerFunc nor httptreemux provides the signature. We need
+	a little bit of customization to satisfy our requirements.
 
 	func (ctx context.Context, w http.ResponseWriter, r *http.Request) error
 */
---
+++ foundation/docker/docker.go
@@ -0,0 +1,98 @@
+// This package provides utilities needed to run a test
+// inside a docker container.
+package docker
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net"
+	"os/exec"
+	"strings"
+)
+
+type Container struct {
+	ID   string
+	Host string // IP:Port
+}
+
+// StopContainer starts the specified container for running tests.
+func StartContainer(image string, port string, dockerArgs []string, appArgs []string) (*Container, error) {
+	args := []string{"run", "-P", "-d"}
+	args = append(args, dockerArgs...)
+	args = append(args, port)
+	args = append(args, appArgs...)
+
+	cmd := exec.Command("docker", args...)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	if err := cmd.Run(); err != nil {
+		return nil, fmt.Errorf("could not start container: %s: %w", image, err)
+	}
+
+	id := out.String()[:12]
+	hostIP, hostPort, err := extractIPPort(id, port)
+	if err != nil {
+		StopContainer(id)
+		return nil, fmt.Errorf("could not extract ip/port: %w", err)
+	}
+
+	c := Container{
+		ID:   id,
+		Host: net.JoinHostPort(hostIP, hostPort),
+	}
+
+	return &c, nil
+}
+
+// StopContainer stops and removes the specified container.
+func StopContainer(id string) error {
+	if err := exec.Command("docker", "stop", id).Run(); err != nil {
+		return fmt.Errorf("could not stop container: %w", err)
+	}
+
+	if err := exec.Command("docker", "rm", id, "-v").Run(); err != nil {
+		return fmt.Errorf("could not remove container: %w", err)
+	}
+
+	return nil
+}
+
+// DumpContainerLogs outputs logs from the running docker container.
+func DumpContainerLogs(id string) []byte {
+	out, err := exec.Command("docker", "logs", id).CombinedOutput()
+	if err != nil {
+		return nil
+	}
+
+	return out
+}
+
+func extractIPPort(id string, port string) (hostIP string, hostPort string, err error) {
+	tmpl := fmt.Sprintf("[{{range $k, $v := (index .NetworkSettings.Ports \"%s/tcp\")}}{{json $v}}{{end}}]", port)
+
+	cmd := exec.Command("docker", "inspect", "-f", tmpl, id)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	if err := cmd.Run(); err != nil {
+		return "", "", fmt.Errorf("could not inspect container %s: %w", id, err)
+	}
+
+	data := strings.ReplaceAll(out.String(), "}{", "},{")
+
+	var docs []struct {
+		HostIP   string
+		HostPort string
+	}
+	if err := json.Unmarshal([]byte(data), &docs); err != nil {
+		return "", "", fmt.Errorf("could not decode json: %w", err)
+	}
+
+	for _, doc := range docs {
+		if doc.HostIP != "::" {
+			return doc.HostIP, doc.HostPort, nil
+		}
+	}
+
+	return "", "", fmt.Errorf("could not locate ip/port")
+}
