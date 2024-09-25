f68fc6d36e21ab5dcf913a264bcafd47dd7b9a99
ameghdadian <amirthedevops@gmail.com>
2024-09-25 13:19:16 +0330

    Add Decode method into foundation layer, so that requests can be decoded using this method

--- foundation/web/requset.go
+++ foundation/web/requset.go
@@ -1,7 +1,31 @@
 package web
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type validator interface {
+	Validate() error
+}
 
 func Param(r *http.Request, key string) string {
 	return r.PathValue(key)
+}
+
+func Decode(r *http.Request, val any) error {
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+	if err := decoder.Decode(val); err != nil {
+		return fmt.Errorf("unable to decode payload: %w", err)
+	}
+
+	if v, ok := val.(validator); ok {
+		if err := v.Validate(); err != nil {
+			return fmt.Errorf("unable to validate payload: %w", err)
+		}
+	}
+
+	return nil
 }
